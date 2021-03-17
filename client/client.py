9890eb10df3b9bb22277572c9aec1686a17addf6
Miguel Santos Batista <miguelsanbat@gmail.com>
2021-03-17 18:06:46 +0100

    Subida parcial envio ficheros

--- client/client.py
+++ client/client.py
@@ -1,12 +1,32 @@
 import socket
+import hashlib
+import json
 
 HOST = '127.0.0.1'
 PORT = 55333
 
+FILENAME = "client/files/example.txt"
+
+def hash_file(file):
+    return "12345"
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'HELLO, world0')
+
+    f = open(FILENAME, "rb")
+    file_data = f.read()
+
+    #data = {"filename" : FILENAME, "file": file_data, "hash":hash_file(file_data), "token":"token"}
+    #dumped_data = json.dumps(data)
+
+    #s.sendall(bytes(dumped_data, encoding="utf-8"))
+    s.sendall(file_data)
     data = s.recv(1024)
 
+    
 
-print('Received' repr(data))
+
+print('Received', repr(data))
+
+
+
--- server/server.py
+++ server/server.py
@@ -3,14 +3,20 @@ import socket
 HOST = '127.0.0.1'
 PORT = 55333
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+FILENAME = "files/example.txt"
+
+while (True):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            f = open(FILENAME, "wb")
+            while True:
+                data = conn.recv(1024)
+                while data:
+                    f.write(data)
+                if not data:
+                    break
+                #conn.sendall(data)
